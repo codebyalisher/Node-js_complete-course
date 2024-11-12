f070caa3c69311da917c14bc733415bed451f098
alisher <alisher.201903953@gcuf.edu.pk>
2024-11-12 14:11:35 +0500

    nodejs complete course

---
+++ what_nd_why_nodejs0.js
@@ -0,0 +1,21 @@
+/*
+What is Nodejs?
+it is a runtime environment and runs on chrome's v8 engine
+language(nodejs is from javascript ,js was dvlped for to make the webpages dynamic but bqckend make the data dynamic but if we ahve to change the page after load then we wil use the js and to make the webpage interactive)
+how does js works(in browser--> it is executed by engine whch we call compiler but to compiel the js engine needs context for execution and context is actually dom)(javascrip-->t)browser-->dom context-->js engine ,since every browser has its own engine so chorme has v8 engine
+ vs runtime mean to run the js ouside dom context is nodejs ,so the context of nodejs is the operating system, mean os-->nodejs-->v8engine so the controll which has os to use those contolls we have to use the os 
+ vast api access of os for nodejs,before we have to only write for browser.
+
+ How Nodejs Works?
+ when we make the server on nodejs so it actually how works,its a software we can say and provide the all functionalities of os and servers ar made form it.it runs on v8 enine which is bascially a c++ code shich runs the js code
+ and so LibUv is also attached through which we can implment the concepts of event loop and thread pool
+
+         .colors{
+            #331C52
+            #291C3A;
+            #8121D0;
+            #DCCFED;
+            #E1EBED
+
+        }
+*/
